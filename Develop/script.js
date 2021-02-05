0189c8ac8131ae81173d3944aaccd9a58ad5717e
Neel Sheth <nsheth109@gmail.com>
2021-02-04 22:44:48 -0500

    added the parameters of the password (special char, numeric char, upper and lowercase)

--- Develop/script.js
+++ Develop/script.js
@@ -29,8 +29,14 @@ var confirmLowerCase;
 function generatePassword() {
   var confirmLength = (prompt("How many characters (between 8-128) would you like your password to contain"));
 
-  // Loop if answer is outside the parameters 
+  // Ask again and let user know if answer is outside the parameters 
   while(confirmLength <= 7 || confirmLength >= 129) {
       alert("Password length must be between 8-128 characters Try again");
       var confirmLength = (prompt("How many characters would you like your password to contain?"));
       } 
+      
+    // Determine parameters of password 
+    var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+    var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+    var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+    var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
