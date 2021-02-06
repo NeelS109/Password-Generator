9cf94aa5be5bb8ec4d4ad1ae17202222a60b6051
Neel Sheth <nsheth109@gmail.com>
2021-02-06 09:55:41 -0500

    added two actions for special char and numbers and concatconated them

--- Develop/script.js
+++ Develop/script.js
@@ -31,19 +31,29 @@ function generatePassword() {
 
   // Ask again and let user know if answer is outside the parameters 
   while(confirmLength <= 7 || confirmLength >= 129) {
-      alert("Password length must be between 8-128 characters Try again");
+      alert("Password length must be between 8-128 characters. Try again");
       var confirmLength = (prompt("How many characters would you like your password to contain?"));
       } 
       
     // Determine parameters of password 
     var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-    var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
-    var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-    var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+    var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numebers");    
+    var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase letters");
+    var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase letters");
       // Loop if answer is outside the parameters 
       while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
         alert("You must choose at least one parameter");
         var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-        var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
-        var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-        var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters"); 
+        var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numbers");    
+        var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase letters");
+        var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase letters"); 
+
+              // Assign an action to the password parameters 
+      var passwordCharacters = []
+      
+      if (confirmSpecialCharacter) {
+        passwordCharacters = passwordCharacters.concat(specialChar)
+      }
+  
+      if (confirmNumericCharacter) {
+        passwordCharacters = passwordCharacters.concat(number)
